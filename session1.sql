988ce7d435231cbb0945bfbcae94a10aba16c9f3
1ss1 <aybilenden@gmail.com>
2021-12-11 21:42:58 +0300

    2. ders eklentileri girildi

--- session1.sql
+++ session1.sql
@@ -38,3 +38,39 @@
 	SELECT * 
 	FROM invoices 
 	WHERE total>10;
+	/*===================================================
+							LIMIT
+====================================================*/
+ 	/*invoices tablosunda Total değeri 10$'dan büyük olan ilk 4 kayıt'ın InvoiceId, 
+	InvoiceDate ve total bilgilerini sorgulayiniz */
+	
+	SELECT InvoiceId, InvoiceDate, total
+	FROM invoices
+	WHERE Total>10
+	LIMIT 4;
+	
+	/*===================================================
+							ORDER BY
+	====================================================*/
+	
+	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtları Total değerine göre 
+	ARTAN sırada sıralayarak sorgulayiniz */
+	
+	SELECT *
+	FROM invoices
+	WHERE total>10
+	ORDER BY total asc ;
+	
+	SELECT *
+	FROM invoices
+	WHERE total>10
+	ORDER BY total;
+	
+	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtlardan işlem tarihi 
+	(InvoiceDate) 	en yeni olan 10 kaydın tüm bilgilerini listeyiniz */ 
+	
+	SELECT *
+	FROM invoices
+	WHERE Total>10
+	ORDER BY InvoiceDate DESC
+	LIMIT 10 ;
